bd47c2058f3c20988ba072d21975c08b9e09c115
janithyasirum <janithyash@gmail.com>
2024-11-13 12:08:07 +0530

    more files changed

--- anushka/admin.py
+++ anushka/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import drawing2d3d, printing3d, ProductRendering, Lasercutting, CNCcutting, Sheetmetal, Metalstrture, Furnitureandtoys
 # Register your models here.
 
+
 admin.site.register(drawing2d3d)
 
 admin.site.register(printing3d)
--- anushka/models.py
+++ anushka/models.py
@@ -4,18 +4,22 @@ from django.core.validators import URLValidator
 
 # Create your models here.
 class drawing2d3d(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img")
+    image1 = models.ImageField(upload_to="img")
+    image2 = models.ImageField(upload_to="img", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
         return self.caption
 
 class Furnitureandtoys(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
@@ -23,9 +27,11 @@ class Furnitureandtoys(models.Model):
 
 
 class printing3d(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
@@ -33,9 +39,11 @@ class printing3d(models.Model):
 
 
 class ProductRendering(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
@@ -43,36 +51,44 @@ class ProductRendering(models.Model):
 
 
 class Lasercutting(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
         return self.caption
 
 class CNCcutting(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
         return self.caption
 
 class Sheetmetal(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
         return self.caption
 
 class Metalstrture(models.Model):
-    caption = models.CharField(max_length=300)
+    caption = models.CharField(max_length=70)
     description = models.TextField()
-    image = models.ImageField(upload_to="img_profiles")
+    image1 = models.ImageField(upload_to="img_profiles")
+    image2 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
+    image3 = models.ImageField(upload_to="img_profiles", blank=True, null=True)
     link = models.URLField(blank=True, validators=[URLValidator])
 
     def __str__(self):
